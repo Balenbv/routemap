70106be3fc6b271f80287b31b59232bcf5fb896a
DiegoAvila1 <diegoavilaaa0@gmail.com>
2025-02-16 21:05:37 -0300

    -Agrege el mapa con un marcador , -agrege la libreria de react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
+function Map() {
+  const position = [-38.9516, -68.0591]; // Coordenadas de Buenos Aires, Argentina
+
   return (
-    <div className="App">
-      <h1> Hello World aaaaa </h1>
-    </div>
+    <MapContainer center={position} zoom={13} style={{ height: '100vh', width: '100vw' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          Mi gente tamo en japon 
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
-export default App;
+export default Map;
